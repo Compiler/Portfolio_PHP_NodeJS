f2363f78d37c84705ecf84cc0a6a947ebde01a2f
compiler <lukerochedev@gmail.com>
2021-06-17 09:55:39 -0400

    php file

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require('http') //include http
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res){
-    res.write("Hello from res")
-    res.end()
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+    fs.readFile("index.php", function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write("Error while finding html:", error)
+        }else{
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error){
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> PHP start </p>
+    <?php
+    if(false){
+        echo "<p> P tag </p>";
+    }
+    ?>
+    <p> PHP End </p>
+</body>
+</html>
