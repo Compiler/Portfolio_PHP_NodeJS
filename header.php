80b31b0d69f21fb80c8079bb0b9e1c86f04a6930
dingas0987 <vincentliang424@gmail.com>
2021-06-23 15:15:12 -0400

    dumb logo but it works

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="container_12">
   <div class="grid_12">
-    <h1><a href="index.php"><img src="images/logo.png" alt=""></a></h1>
+    <h1><a href="index.php"><img src="images/restaurant_logo.png" alt=""></a></h1>
     <div class="menu_block">
       <nav>
         <ul class="sf-menu">
