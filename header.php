6004d1e8a7cc91d8a5cd57bd3bd7fb240526844a
dingas0987 <vincentliang424@gmail.com>
2021-06-22 23:54:33 -0400

    changed to business site

---
+++ header.php
@@ -0,0 +1,23 @@
+<div class="container_12">
+  <div class="grid_12">
+    <h1><a href="index.php"><img src="images/logo.png" alt=""></a></h1>
+    <div class="menu_block">
+      <nav>
+        <ul class="sf-menu">
+          <li><a href="index.php">Home</a></li>
+          <li class="current"><a href="about-us.php">About Us</a>
+            <ul>
+              <li><a href="#"> cuisine</a></li>
+              <li><a href="#">Good rest</a></li>
+              <li><a href="#">Services</a></li>
+            </ul>
+          </li>
+          <li><a href="menu.php">Menu</a></li>
+          <li><a href="contacts.php">Contact Us</a></li>
+        </ul>
+      </nav>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
