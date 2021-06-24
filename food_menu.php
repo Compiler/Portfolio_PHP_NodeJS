56b90f1f4a46498d353bf762e814997fc787053f
dingas0987 <vincentliang424@gmail.com>
2021-06-23 21:23:24 -0400

    menu changes

---
+++ food_menu.php
@@ -0,0 +1,55 @@
+<h2>Our Menu</h2>
+        <p class="col2 inn1"><a href="#">food</a></p>
+        <p>there be a variety of food </p>
+        <div class="menu">
+          <h2>Breakfast</h2>
+          <div class="grid_3 omega"> <img src="food pics/egg_florentine_1.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Eggs Florentine</a></h3>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/sausage_gravy_biscuits_egg_5.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Sausage Gravy Biscuits</a></h3>
+          </div>
+
+          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+          <h2>Lunch</h2>
+          <div class="grid_3 omega"> <img src="food pics/salmon_burger_2.jpg" alt="" class="img_inner">
+            <h3><a href="#">Salmon Burger</a></h3>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/beef_birra_taco_3.jpg" alt="" class="img_inner">
+            <h3><a href="#">Beef Birra Tacos</a></h3>
+          </div>
+
+          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+          <h2>Dinner</h2>
+          <div class="grid_3 omega"> <img src="food pics/chicken_katsu_curry_3.jpg" alt="" class="img_inner">
+            <h3><a href="#">Chicken Katsu Curry</a></h3>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/ramen.jpg" alt="" class="img_inner">
+            <h3><a href="#">Ramen</a></h3>
+            <p>FOOD</p>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/general_tsaos_2.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Liotymer</a></h3>
+          </div>
+
+          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+          <h2>Dessert</h2>
+          <div class="grid_3 omega"> <img src="food pics/lava_cake_3.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Lava Cake</a></h3>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/keylime_pie_2.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Key Lime Pie</a></h3>
+          </div>
+
+          <div class="grid_3 omega"> <img src="food pics/japanese_cheese_cake_2.jpg" alt="" class="img_inner">
+            <h3 class="mb0"><a href="#">Japanese Cheese Cake</a></h3>
+          </div>
+        </div>
+        <div class="clear"></div>
--- menu.php
+++ menu.php
@@ -22,60 +22,10 @@
   </header> 
   <div class="content">
     <div class="container_12">
-      <div class="grid_6">
-        <h2>Our Menu</h2>
-        <p class="col2 inn1"><a href="#">Integer non metus justo ac posuere turpis. Integer iaculis porta metus, in aliquam orci vehicula sit amet. Suspendisse turpis mauris, laoreet ac aliquet a lobortis in massa. Etiam tortor metus, tincidunt vel congue etu</a></p>
-        <p>Aenean sed nunc lorem, nec ornare nibh. Pellentesque vitae sem odio. Vivamus vulputate est eget elit rutrum a consectetur justo aliquet. Aliquam hendrerit consectetur mi, non molestie massa venenatis quis. Integer aliquet euismod nisl, et sagittis sem scelerisque sit amet. Etiam dapibus, tellus et imperdiet pulvinar, urna nunc consectetur enim, in </p>
-        <div class="menu">
-          <div class="grid_3 alpha"> <img src="images/page3_img1.jpg" alt="" class="img_inner">
-            <h3><a href="#">Donec</a></h3>
-          </div>
-          <div class="grid_3 omega"> <img src="images/page3_img2.jpg" alt="" class="img_inner">
-            <h3><a href="#">Murtyio</a></h3>
-          </div>
-          <div class="grid_3 alpha"> <img src="images/page3_img3.jpg" alt="" class="img_inner">
-            <h3><a href="#">Holity as Polfh</a></h3>
-          </div>
-          <div class="grid_3 omega"> <img src="images/page3_img4.jpg" alt="" class="img_inner">
-            <h3><a href="#">Kiperty</a></h3>
-          </div>
-          <div class="grid_3 alpha"> <img src="images/page3_img5.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Gyloit</a></h3>
-          </div>
-          <div class="grid_3 omega"> <img src="images/page3_img6.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Liotymer</a></h3>
-          </div>
-        </div>
-        <div class="clear"></div>
-      </div>
-      <div class="grid_5 prefix_1">
-        <h2 class="head2">Chef’s Secrets</h2>
-        <ul class="list l1">
-          <li><a href="#"> Maecenas malesuada sapien</a></li>
-          <li><a href="#">Etiam tortor metus, tincidunt vel gtcongue</a></li>
-          <li><a href="#">Pellentesque vitae sem odio</a></li>
-          <li><a href="#"> Lorem ipsum dolor sit amet, con</a></li>
-          <li><a href="#">Provide preferred vendor's list</a></li>
-          <li><a href="#">Vivamus vulputate est eget elit s tellus et</a></li>
-          <li><a href="#"> Nam blandit metus eget pretium</a></li>
-          <li><a href="#"> Aaecenas koalesuada sapien</a></li>
-          <li><a href="#">Ttiam tortor metus tiklioidunt vel</a></li>
-          <li><a href="#">Eellentesque vitae sem odiokhoy</a></li>
-          <li><a href="#"> Okoem ipsum dolor sit amet df</a></li>
-          <li><a href="#">Rovide preferred vendor's listko frefrwq hu</a></li>
-          <li><a href="#">Vvamus vulputate est eet elit</a></li>
-          <li class="mb0"><a href="#"> Am blandit metus eet pretium</a></li>
-        </ul>
-        <h2 class="head1">Wine List</h2>
-        <ul class="list">
-          <li><a href="#"> Koalesuada  libero</a></li>
-          <li><a href="#">Ortor metus vel</a></li>
-          <li><a href="#">Eellentesque vitae sem</a></li>
-          <li><a href="#"> Ipsum</a></li>
-          <li><a href="#">Rovide preferred</a></li>
-          <li><a href="#">Vulputate est</a></li>
-          <li><a href="#"> Bandit metus</a></li>
-        </ul>
+      <div class="grid_menu">
+        <?php
+          require("food_menu.php");
+        ?>
       </div>
       <div class="clear"></div>
       <div class="bottom_block">
