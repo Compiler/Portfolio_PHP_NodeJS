ed4f664d64ffa86690ea3807db53f304e4ae878a
dingas0987 <vincentliang424@gmail.com>
2021-06-23 22:01:00 -0400

    again...

--- best_selling_display.php
+++ best_selling_display.php
@@ -1,33 +1,33 @@
 <h2>Best Selling</h2>
-  <a href="#" class="prev"></a><a href="#" class="next"></a>
+  <a  class="prev"></a><a  class="next"></a>
   <ul class="carousel1">
     <li>
       <div><img src="food pics/beef_birra_taco_2.jpg" width="220px" height="220px"alt="">
-        <div class="col3"> <a href="#">Beef Birra Tacos</a></div>
+        <div class="col3"> <a>Beef Birra Tacos</a></div>
         <div class="price">$10</div>
       </div>
     </li>
     <li>
       <div><img src="food pics/meatball_sub_2.jpg" alt="">
-        <div class="col3"> <a href="#">Meatball Sub</a></div>
+        <div class="col3"> <a>Meatball Sub</a></div>
         <div class="price">$8</div>
       </div>
     </li>
     <li>
       <div><img src="food pics/salmon_burger_3.jpg" alt="">
-        <div class="col3"> <a href="#">Salmon Burger</a></div>
+        <div class="col3"> <a>Salmon Burger</a></div>
         <div class="price">$12</div>
       </div>
     </li>
     <li>
       <div><img src="food pics/sausage_gravy_biscuits_egg_5.jpg" alt="">
-        <div class="col3"> <a href="#">Sausage Gravy Biscuits</a></div>
+        <div class="col3"> <a>Sausage Gravy Biscuits</a></div>
         <div class="price">$8</div>
       </div>
     </li>
     <li>
       <div><img src="food pics/general_tsos_2.jpg" alt="">
-        <div class="col3"> <a href="#">General Tsos Chicken</a></div>
+        <div class="col3"> <a>General Tsos Chicken</a></div>
         <div class="price">$5</div>
       </div>
     </li>
