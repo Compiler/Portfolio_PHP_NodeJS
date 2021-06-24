a562d5ffa90440f866bba2d7bb494a8cc2c93a98
dingas0987 <vincentliang424@gmail.com>
2021-06-23 21:59:58 -0400

    removed links from menu

--- food_menu.php
+++ food_menu.php
@@ -7,12 +7,12 @@
         <div class="menu">
           <h2>Breakfast</h2>
           <div class="grid_3 omega"> <img src="food pics/egg_florentine_1.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Eggs Florentine</a></h3>
+            <h3 class="mb0"><a>Eggs Florentine</a></h3>
             <p> $5 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/sausage_gravy_biscuits_egg_5.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Sausage Gravy Biscuits</a></h3>
+            <h3 class="mb0"><a>Sausage Gravy Biscuits</a></h3>
             <p> $8 </p>
           </div>
 
@@ -20,17 +20,17 @@
 
           <h2>Lunch</h2>
           <div class="grid_3 omega"> <img src="food pics/salmon_burger_2.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Salmon Burger</a></h3>
+            <h3 class="mb0"><a>Salmon Burger</a></h3>
             <p> $12</p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/beef_birra_taco_3.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Beef Birra Tacos</a></h3>
+            <h3 class="mb0"><a>Beef Birra Tacos</a></h3>
             <p> $10 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/meatball_sub_2.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Meatball Sub</a></h3>
+            <h3 class="mb0"><a>Meatball Sub</a></h3>
             <p> $8 </p>
           </div>
 
@@ -38,34 +38,34 @@
 
           <h2>Dinner</h2>
           <div class="grid_3 omega"> <img src="food pics/chicken_katsu_curry_3.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Chicken Katsu Curry</a></h3>
+            <h3 class="mb0"><a>Chicken Katsu Curry</a></h3>
             <p> $7 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/ramen.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Ramen</a></h3>
+            <h3 class="mb0"><a>Ramen</a></h3>
             <p> $8 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/general_tsos_2.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">General Tsos Chicken</a></h3>
+            <h3 class="mb0"><a>General Tsos Chicken</a></h3>
             <p> $5 </p>
           </div>
 
           <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
           <h2>Dessert</h2>
           <div class="grid_3 omega"> <img src="food pics/lava_cake_3.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Lava Cake</a></h3>
+            <h3 class="mb0"><a>Lava Cake</a></h3>
             <p> $2 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/keylime_pie_2.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Key Lime Pie</a></h3>
+            <h3 class="mb0"><a>Key Lime Pie</a></h3>
             <p> $2 </p>
           </div>
 
           <div class="grid_3 omega"> <img src="food pics/japanese_cheese_cake_2.jpg" alt="" class="img_inner">
-            <h3 class="mb0"><a href="#">Japanese Cheese Cake</a></h3>
+            <h3 class="mb0"><a>Japanese Cheese Cake</a></h3>
             <p> $3 </p>
           </div>
         </div>
